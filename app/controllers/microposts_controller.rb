3d8a86f64ddf3a03b31390632cf09dbc93d1d9e0
shimizu <simichan1976@gmail.com>
2016-09-26 19:07:31 +0000

    add posting microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -11,6 +11,14 @@ before_action :logged_in_user, only: [:create]
     end
   end
   
+   def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    return redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
+  end
+  
   private
   def micropost_params
     params.require(:micropost).permit(:content)
