22a41fb018a1d8a48b5b29df52735ec1a1871f35
shimizu <simichan1976@gmail.com>
2016-09-26 13:55:51 +0000

    using boatstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,26 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to  user_path(@user), notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :profile, :password,
                                  :password_confirmation)
   end
 end
