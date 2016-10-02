9583b393809f3dcf7587d701df737b2a2c88229a
shimizu <simichan1976@gmail.com>
2016-10-02 09:13:38 +0000

    carrierwave

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base  
+
+  # RMagickをincludeします   
+    include CarrierWave::RMagick
+    
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  process convert: 'jpg'
+  
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+ # 画像の上限を700pxにする
+  process :resize_to_limit => [700, 700]
+
+  # サムネイルを生成する設定
+  version :thumb do
+    process :resize_to_limit => [300, 300]
+  end
+
+end
